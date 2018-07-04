eadda0cb55adea6bf5c65fb359b665de5ec06428
akshaya9 <32271596+akshaya9@users.noreply.github.com>
2018-07-04 08:51:17 +0530

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -3,6 +3,6 @@ from credentials import account_sid, outh_token, my_cell, my_twilio
 
 client = Client(account_sid, outh_token)
 
-my_msg = ''.join(['Bloody hell!!\n' for i in range(9)])
+my_msg = ''.join(['Your text goes here...\n' for i in range(9)])
 
 message = client.messages.create(to=my_cell, from_=my_twilio, body=my_msg)
